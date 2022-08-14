96f743284695ec59242c86472c171c9059996834
Felipe Sousa Developer <fesousalocutor@gmail.com>
2022-08-14 00:24:23 -0300

    feat: concluded Error Handler

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -2,8 +2,10 @@ import { Router } from 'express';
 
 const routes = Router();
 
+const author = 'Felipe Fernandes';
+
 routes.get('/', (request, response) => {
-  response.json({ msg: 'Welcome! API' });
+  response.json({ msg: 'Welcome! API sucessfully up!', author });
 });
 
 export default routes;
--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,6 +1,7 @@
 import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import routes from './routes';
+import AppError from '../errors/AppError';
 
 const app = express();
 
@@ -10,12 +11,18 @@ app.use(express.json());
 app.use(routes);
 
 app.use(
-  (
-    error: Error,
-    request: Request,
-    response: Response,
-    next: NextFunction,
-  ) => {},
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal Server Error',
+    });
+  },
 );
 
 app.listen(3333, () => console.log(`API is running on port 3333`));
